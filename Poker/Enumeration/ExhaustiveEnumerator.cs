b24c731863542c014308bffa025b038caa97a8a1
Arturas Drumsta <drumsta@gmail.com>
2015-08-24 11:57:44 +0100

    Added exhaustive and Monte Carlo (random) enumerators

---
+++ Poker/Enumeration/ExhaustiveEnumerator.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Poker.Deck;
+
+namespace Poker.Enumeration
+{
+    /// <summary>
+    ///     Exhaustively enumerates all possible card outcomes.
+    /// </summary>
+    public sealed class ExhaustiveEnumerator : IEnumerator<CardMask>
+    {
+        private const int DefaultCardsToEnumerate = 2;
+        private const int MinCardsToEnumerate = 1;
+        private const int MaxCardsToEnumerate = 9;
+
+        private CardMask[] _cardMask;
+
+        private readonly int _noOfCardsInDeck;
+        private readonly int _cardsToEnumerate;
+        private readonly CardMask _deadMask;
+        private readonly Func<int, CardMask> _toCardMask;
+        private bool _disposed;
+        private bool _isDeadEnd;
+        private int _currentLevel;
+        private int[] _index;
+
+        public ExhaustiveEnumerator(int noOfCardsInDeck,
+            int cardsToEnumerate,
+            CardMask deadMask,
+            Func<int, CardMask> toCardMask)
+        {
+            _noOfCardsInDeck = noOfCardsInDeck;
+            _cardsToEnumerate = cardsToEnumerate < MinCardsToEnumerate || cardsToEnumerate > MaxCardsToEnumerate
+                ? DefaultCardsToEnumerate
+                : cardsToEnumerate;
+            _deadMask = deadMask;
+            _toCardMask = toCardMask;
+
+            Reset();
+        }
+
+        /// <summary>
+        ///     Gets the element in the collection at the current position of the enumerator.
+        /// </summary>
+        /// <value>
+        ///     The element in the collection at the current position of the enumerator.
+        /// </value>
+        public CardMask Current { get; private set; }
+
+        /// <summary>
+        ///     Gets the current element in the collection.
+        /// </summary>
+        /// <value>
+        ///     The current element in the collection.
+        /// </value>
+        object IEnumerator.Current => Current;
+
+        /// <summary>
+        ///     Performs application-defined tasks associated with freeing, releasing, or resetting
+        ///     unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        ///     Advances the enumerator to the next element of the collection.
+        /// </summary>
+        /// <returns>
+        ///     <c>true</c> if the enumerator was successfully advanced to the next element;
+        ///     <c>false</c> if the enumerator has passed the end of the collection.
+        /// </returns>
+        public bool MoveNext()
+        {
+            if (_isDeadEnd)
+            {
+                return false;
+            }
+
+            var isEnd = false;
+            do
+            {
+                if (_index[_currentLevel] <= 0)
+                {
+                    if (_currentLevel == 0)
+                    {
+                        _isDeadEnd = true;
+                        continue;
+                    }
+
+                    --_currentLevel;
+                    if (_index[_currentLevel] <= 0)
+                    {
+                        continue;
+                    }
+                }
+
+                --_index[_currentLevel];
+                var tempCardMask = _toCardMask(_index[_currentLevel]);
+
+                if (CardMask.IsAnySameCardSet(_deadMask, tempCardMask))
+                {
+                    continue;
+                }
+
+                _cardMask[_currentLevel] = _currentLevel == 0
+                    ? tempCardMask
+                    : _cardMask[_currentLevel - 1] | tempCardMask;
+
+                ++_currentLevel;
+                isEnd = _currentLevel == _cardsToEnumerate;
+
+                if (!isEnd)
+                {
+                    _index[_currentLevel] = _index[_currentLevel - 1];
+                }
+            } while (!isEnd && !_isDeadEnd);
+
+            --_currentLevel;
+
+            Current = _isDeadEnd ? CardMask.Empty : _cardMask[_currentLevel];
+
+            return !_isDeadEnd;
+        }
+
+        /// <summary>
+        ///     Sets the enumerator to its initial position, which is before the first element in the
+        ///     collection.
+        /// </summary>
+        public void Reset()
+        {
+            Current = CardMask.Empty;
+            _isDeadEnd = false;
+            _currentLevel = 0;
+            _index = new int[_cardsToEnumerate];
+            _cardMask = new CardMask[_cardsToEnumerate];
+            _index[0] = _noOfCardsInDeck;
+        }
+
+        ~ExhaustiveEnumerator()
+        {
+            Dispose(false);
+        }
+
+        /// <summary>
+        ///     Performs application-defined tasks associated with freeing, releasing, or resetting
+        ///     unmanaged resources.
+        /// </summary>
+        /// <param name="disposing">
+        ///     Whether to dispose all managed resources as well.
+        /// </param>
+        private void Dispose(bool disposing)
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                // Dispose managed resources.
+                _index = null;
+                _cardMask = null;
+            }
+
+            // Dispose un-managed resources.
+            _disposed = true;
+        }
+    }
+}
---
+++ Poker/Enumeration/RandomEnumerator.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Poker.Deck;
+
+namespace Poker.Enumeration
+{
+    public sealed class RandomEnumerator : IEnumerator<CardMask>
+    {
+        private const long DefaultRandomTrials = 1L;
+        private const int DefaultCardsToEnumerate = 2;
+        private const int MinCardsToEnumerate = 1;
+        private const int MaxCardsToEnumerate = 9;
+        private readonly int _cardsToEnumerate;
+        private readonly CardMask _deadMask;
+
+        private readonly int _noOfCardsInDeck;
+
+        private readonly Random _random = new Random();
+        private readonly Func<int, CardMask> _toCardMask;
+        private readonly long _totalTrials;
+        private long _currentTrial;
+        private bool _disposed;
+        private bool _isDeadEnd;
+
+        public RandomEnumerator(int noOfCardsInDeck,
+            int cardsToEnumerate,
+            CardMask deadMask,
+            Func<int, CardMask> toCardMask)
+        {
+            _noOfCardsInDeck = noOfCardsInDeck;
+            _cardsToEnumerate = cardsToEnumerate < MinCardsToEnumerate || cardsToEnumerate > MaxCardsToEnumerate
+                ? DefaultCardsToEnumerate
+                : cardsToEnumerate;
+            _deadMask = deadMask;
+            _toCardMask = toCardMask;
+            _totalTrials = DefaultRandomTrials;
+
+            Reset();
+        }
+
+        /// <summary>
+        ///     Gets the element in the collection at the current position of the enumerator.
+        /// </summary>
+        /// <value>
+        ///     The element in the collection at the current position of the enumerator.
+        /// </value>
+        public CardMask Current { get; private set; }
+
+        /// <summary>
+        ///     Gets the current element in the collection.
+        /// </summary>
+        /// <value>
+        ///     The current element in the collection.
+        /// </value>
+        object IEnumerator.Current => Current;
+
+        /// <summary>
+        ///     Performs application-defined tasks associated with freeing, releasing, or resetting
+        ///     unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        ///     Advances the enumerator to the next element of the collection.
+        /// </summary>
+        /// <returns>
+        ///     <c>true</c> if the enumerator was successfully advanced to the next element;
+        ///     <c>false</c> if the enumerator has passed the end of the collection.
+        /// </returns>
+        public bool MoveNext()
+        {
+            if (_isDeadEnd)
+            {
+                return false;
+            }
+
+            ++_currentTrial;
+            if (_currentTrial == _totalTrials)
+            {
+                _isDeadEnd = true;
+            }
+
+            Current = CardMask.Empty;
+            var currentDeadMask = _deadMask;
+            for (var i = 0; i != _cardsToEnumerate; ++i)
+            {
+                CardMask tempCardMask;
+                do
+                {
+                    var randomIndex = _random.Next(_noOfCardsInDeck);
+                    tempCardMask = _toCardMask(randomIndex);
+                } while (CardMask.IsAnySameCardSet(currentDeadMask, tempCardMask));
+
+                Current |= tempCardMask;
+                currentDeadMask |= tempCardMask;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        ///     Sets the enumerator to its initial position, which is before the first element in the
+        ///     collection.
+        /// </summary>
+        public void Reset()
+        {
+            Current = CardMask.Empty;
+            _isDeadEnd = false;
+            _currentTrial = 0;
+        }
+
+        ~RandomEnumerator()
+        {
+            Dispose(false);
+        }
+
+        /// <summary>
+        ///     Performs application-defined tasks associated with freeing, releasing, or resetting
+        ///     unmanaged resources.
+        /// </summary>
+        /// <param name="disposing">
+        ///     Whether to dispose all managed resources as well.
+        /// </param>
+        private void Dispose(bool disposing)
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                // Dispose managed resources.
+            }
+
+            // Dispose un-managed resources.
+            _disposed = true;
+        }
+    }
+}
